49b0b642d97f89b9890fb0e09722cf55682a57fb
Lynnn17 <lintangkusuma17@gmail.com>
2024-11-18 11:17:19 +0700

    tes2

---
+++ src/views/Tes.jsx
@@ -0,0 +1,10 @@
+const Tes = () => {
+  return (
+    <img
+      src="/proxy-image/uc?export=view&id=1CizSM4Oy_niz4NW_8VgAR2v1cCz4jbTw"
+      alt="Deskripsi Gambar"
+    />
+  );
+};
+
+export default Tes;
