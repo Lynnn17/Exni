6eab59006b21f404d7704b8c557d9de62573160b
Daamleon <withdaamleon@gmail.com>
2024-11-25 09:24:49 +0700

    update admin  dashboard

--- src/components/admin/Dashboard.jsx
+++ src/components/admin/Dashboard.jsx
@@ -1,6 +1,7 @@
 import { FaUsers } from "react-icons/fa6";
 import { TbBuildingWarehouse } from "react-icons/tb";
 import { GiReceiveMoney } from "react-icons/gi";
+import { FaHandshakeSimple } from "react-icons/fa6";
 import Card from "../reusable/card/CardDashboard";
 import React from "react";
 const Dashboard = () => {
@@ -10,10 +11,11 @@ const Dashboard = () => {
         <div className="w-full p-4 bg-white mt-4 h-full lg:h-screen">
           <p className="text-lg uppercase font-medium">Dashboard</p>
           <div className="w-full h-[1px] bg-teks mt-2"> </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 pt-4">
-            <Card count="169" label="User" icon={FaUsers} />
-            <Card count="169" label="Assets" icon={TbBuildingWarehouse} />
-            <Card count="169" label="Transactions" icon={GiReceiveMoney} />
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 pt-4">
+            <Card count="169" label="Akun" icon={FaUsers} />
+            <Card count="169" label="Aset Sewa" icon={TbBuildingWarehouse} />
+            <Card count="169" label="Pengajuan" icon={FaHandshakeSimple} />
+            <Card count="169" label="Transaaksi" icon={GiReceiveMoney} />
           </div>
         </div>
       </main>
