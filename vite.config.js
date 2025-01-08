d3ddfde2215aa7246b3231c177d98127ec1e567e
Lynnn17 <lintangkusuma17@gmail.com>
2025-01-08 11:12:39 +0700

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dotenv from "dotenv";
+
 dotenv.config();
+
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: "terser", // Menggunakan terser untuk minifikasi
+    terserOptions: {
+      format: {
+        comments: false, // Menghapus semua komentar
+      },
+      compress: {
+        drop_console: true, // Menghapus console.log
+        drop_debugger: true, // Menghapus debugger
+      },
+    },
+  },
 });
